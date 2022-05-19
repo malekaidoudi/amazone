c4c7c8550aa10cd3c05444e43a0d71550792aa15
malekaidoudi <malekaidoudi@gmail.com>
2022-05-19 19:54:19 +0200

    add bootstrap & update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,33 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/homeScreen/HomeScreen";
 import ProductScreen from "./screens/productScreen/ProductScreen";
+import { Container, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 function App() {
   return (
     <Router>
-      <header>
-        <Link to="/">Amazone</Link>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/product/:slug" element={<ProductScreen />} />
-        </Routes>
-      </main>
+      <div className="d-flex flex-column container-site">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Amazone</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
     </Router>
   );
 }
